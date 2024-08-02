a6dc7201322ea95e2248e7c5d936ba8046a9ff3d
delsonscyan <delsonscyan@hkjccorpdev.onmicrosoft.com>
2024-08-02 01:11:48 +0000

    submit

---
+++ src/elson/data_clean/column_transformer.py
@@ -0,0 +1,6 @@
+
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col
+
+def string_transformer(df:DataFrame,column:col)->DataFrame:
+    pass
--- src/elson/utils/config_helper.py
+++ src/elson/utils/config_helper.py
@@ -1,8 +1,120 @@
-# -*- coding: utf-8 -*-
-# @author: Elson Yan
-# @file: config_helper.py
-# @time: 2024/7/28 17:40
+import yaml, os, json
+
+__helper__ = """
+Note:
+    you can get you you env configration by using this util lib
+eg: contents in '/Workspace/ETL/common_module/config/dev_config.yaml'
+    ETLADLDBServer:
+        driver: "{ODBC Driver 17 for SQL Server}"
+    demo as below:
+    +---------------------------------------------------+
+    |>>> from etl_framework.config_helper import get_env|
+    |    config = get_env()                             |
+    |    driver = config.ETLADLDBServer.driver          |
+    |    print(driver)                                  |
+    +---------------------------------------------------+
+    |... "{ODBC Driver 17 for SQL Server}"              |
+    +---------------------------------------------------+
+"""
+
+
+class EnvUtilError(Exception):
+    """
+    An error occurred at the EnvUtil level.
+    created by Elson Yan
+    """
+
+    def __init__(
+            self,
+            message: str = None,
+            error_caught: str = None,
+    ):
+        """
+        Initialize a new instance of the EnvUtilError class.
+
+        Args:
+        message (str): A message that describes the error.
+        error_caught (str): The exception that causing this exception happened.
+        """
+        self.message = message
+        self.error_caught = error_caught
+
+    def __str__(self):
+        return (
+            ", ".join([
+                "EnvUtil: Failed",
+                " - Message: " + str(self.message),
+                " - error_caught: " + str(self.error_caught)
+            ])
+        )
+
+
+class ParseYamlError(EnvUtilError):
+    def __init__(
+            self,
+            message: str = None,
+            error_caught: str = None,
+    ):
+        super().__init__(message, error_caught)
+
+
+class YamlNotFoundError(EnvUtilError):
+    def __init__(
+            self,
+            message: str = None,
+            error_caught: str = None,
+    ):
+        super().__init__(message, error_caught)
+
+
+class EnvSwitchError(EnvUtilError):
+    def __init__(
+            self,
+            message: str = None,
+            error_caught: str = None,
+    ):
+        super().__init__(message, error_caught)
+
+
+class MultiLevelDictToClass(object):
+    def __init__(self, *args):
+        for arg in args:
+            for k, v in arg.items():
+                if isinstance(v, dict):
+                    self.__dict__[k] = MultiLevelDictToClass(v)
+                else:
+                    self.__dict__[k] = v
+
+    def __str__(self) -> str:
+        return json.dumps(self, default=lambda o: o.__dict__, indent=4)
+
+
+def get_config_yaml():
+    env = os.getenv("ENV", default="DEV")
+    config_path = f"/Workspace/ETL/common_module/config/{env.lower()}_config.yaml"
+    if env.lower() in ["dev", "sat", "prod", "debug"]:
+        if not os.path.exists(config_path):
+            return YamlNotFoundError(message=f"{config_path} not found",
+                                     error_caught="this is a message from func get_config_yaml()", )
+        try:
+            with open(config_path, mode="r", encoding="UTF-8") as file:
+                env_objs = yaml.load(file, Loader=yaml.FullLoader)
+            return env_objs
+        except Exception as e:
+            return ParseYamlError(message=f"Error raised when parsing {config_path}",
+                                  error_caught=e, )
+    else:
+        return EnvSwitchError(message=f"ENV in os should be one of them: ('dev', 'sat', 'prod', 'debug') current: {env}",
+                              error_caught="this is a message from func get_config_yaml()")
 
 
 def get_env():
-    pass
+    env_objs = get_config_yaml()
+    if issubclass(type(env_objs), EnvUtilError):
+        return env_objs
+    return MultiLevelDictToClass(env_objs)
+
+# if __name__ == '__main__':
+#     env_objs = get_config_yaml()
+#     info = get_env()
+#     print(str(info))
