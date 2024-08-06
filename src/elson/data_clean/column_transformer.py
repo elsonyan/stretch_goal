5786a3c39a03cd63ebf13af538f10b43da38c917
delsonscyan <delsonscyan@hkjccorpdev.onmicrosoft.com>
2024-08-06 09:08:25 +0000

    aaa

--- src/elson/data_clean/column_transformer.py
+++ src/elson/data_clean/column_transformer.py
@@ -105,7 +105,7 @@ class Cleansing:
             plan: exec_plan = self.exec_plan_list.shift
             if not plan:
                 break
-            print("executing plan:",plan)
+            print("execution plan:",plan)
             self.df = plan.exec(self.df)
         return self.df
 
